342a6e3252eb355a96720636c4ada0e9c8def7fc
Summer 2015 <dawes@pacificu.edu>
2016-02-10 11:14:05 -0800

    changed buffer flush behavior

--- coincidence.py
+++ coincidence.py
@@ -2,34 +2,24 @@ import serial
 import struct
 import array
 from bitstring import BitArray
-import matplotlib.pyplot as plt
 import time
 
-fig=plt.figure()
-plt.axis([0,10,0,50000])
-
-s = serial.Serial("/dev/ttyUSB2",19200,bytesize=serial.SEVENBITS,stopbits=serial.STOPBITS_ONE)
+s = serial.Serial("/dev/ttyUSB0",19200,bytesize=serial.SEVENBITS,stopbits=serial.STOPBITS_ONE)
 
 buffer = []
+s.flushInput()
 while True:
     try:
-        data = s.read()
-        if data == "\x7f":
-            bits = BitArray(bytes=buffer)
-	    # test length:
-	    if bits.length == 8*40:
-		time.sleep(0.1)
-            	a, b, c, d, c4, c5, c6, c7 = bits.unpack('uintle:40,uintle:40,uintle:40,uintle:40,uintle:40,uintle:40,uintle:40,uintle:40')
-		print a, b, c, d, c4, c5, c6, c7
-		plt.scatter((a,b,c,d,c4,c5,c6,c7),(0,1,2,3,4,5,6,7))
-		plt.draw()
-            else:
-                print "short packet"
-            buffer = []
-        else:
-            buffer.append(data)
+        while s.read() != "\x7f":
+            pass
+        #bytesToRead = s.inWaiting()
+        #if bytesToRead == 41:
+        inbuffer = s.read(41)
+        #print repr(inbuffer)
+        bits = BitArray(bytes=inbuffer[:-1])
+        a, b, c, d, c4, c5, c6, c7 = bits.unpack('uintle:40,uintle:40,uintle:40,uintle:40,uintle:40,uintle:40,uintle:40,uintle:40')
+        print a, b, c, d, c4, c5, c6, c7
     except KeyboardInterrupt:
         print "W: interrupt received, ending data collection"
+        s.close()
         break
-
-s.close()
