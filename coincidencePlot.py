d6c93b13ae4ebeb540c86ff58c299a05139fdbea
Summer 2015 <dawes@pacificu.edu>
2018-03-05 14:46:13 -0800

    working for now

--- coincidencePlot.py
+++ coincidencePlot.py
@@ -13,28 +13,32 @@ scale2 = 1000
 
 plt.ion()
 fig = plt.figure()
+
+index = np.arange(4)
+bar_width = 0.4
+
 axes = fig.add_subplot(121)
 axes2 = fig.add_subplot(122)
+
+# axes.set_xticks(("A", "B", "A'", "B'"))
+# axes2.set_xticks(("C4", "C5", "C6", "C7"))
 # axes.set_autoscale_on(True)
 # axes.autoscale_view(True,True,True)
 # axes2.set_autoscale_on(True)
 # axes2.autoscale_view(True,True,True)
-axes.set_ylim([0,scale1])
-axes2.set_ylim([0,scale2])
+# axes.set_ylim([0,scale1])
+# axes2.set_ylim([0,scale2])
 
 dataset = (1,1,1,1)
 dataset2 = (1,1,1,1)
 
-index = np.arange(4)
-bar_width = 0.3
 
 rects = axes.bar(index,dataset,bar_width)
 rects2 = axes2.bar(index,dataset2,bar_width)
-plt.xticks(index + bar_width/2.0, ("A", "B", "A'", "B'", "C4", "C5", "C6", "C7"))
 
 
 #s = serial.Serial("/dev/ttyS0",19200,bytesize=serial.SEVENBITS,stopbits=serial.STOPBITS_ONE)
-s = serial.Serial("/dev/ttyACM2",250000,timeout=2)
+s = serial.Serial("/dev/ttyACM1",250000,timeout=2)
 
 buffer = []
 s.flushInput()
@@ -48,29 +52,31 @@ while True:
             if True:
                 #time.sleep(0.1)
                 a, b, c, d, c4, c5, c6, c7, err = data.decode('ascii').rstrip().split(' ') # TODO parse as ints
-                print(a, b, c, d, c4, c5, c6, c7)
+                # print(a, b, c, d, c4, c5, c6, c7)
                 newdata = (a, b, c, d)
+                newdata = map(int,newdata)
                 newdata2 = (c4, c5, c6, c7)
+                newdata2 = map(int,newdata2)
                 for rect, h in zip(rects, newdata):
                     rect.set_height(h)
 
                 for rect2, h in zip(rects2, newdata2):
                     rect2.set_height(h)
+
                 if int(a) > scale1 or  int(b) > scale1:
-                    scale1 = 1.2*max(a,b)
+                    scale1 = 1.2*max(int(a),int(b))
                     axes.set_ylim([0,scale1])
                 elif int(a) < (scale1/2) and int(b) < (scale1/2):
-                    scale1 = 1.2*max(a,b)
+                    scale1 = 1.2*max(int(a),int(b))
                     axes.set_ylim([0,scale1])
-                if int(c4) > scale2:
-                    scale2 = c4*1.2
+                if int(c4) > scale2 or int(c5) > scale2:
+                    scale2 = 1.2*max(int(c4),int(c5))
                     axes2.set_ylim([0,scale2])
-                elif int(c4) < (scale2/2):
-                    scale2 = c4*1.2
+                elif int(c4) < (scale2/2) or int(c5) < scale2:
+                    scale2 = 1.2*max(int(c4),int(c5))
                     axes2.set_ylim([0,scale2])
                 # axes.relim()
                 # axes.autoscale_view(True,True,True)
-                #
                 # axes2.relim()
                 # axes2.autoscale_view(True,True,True)
 
