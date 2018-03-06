6a0dadad4baa8d354e0533efa034288a47acd76f
Andrew Dawes <dawes@pacificu.edu>
2018-03-06 01:00:44 -0800

    working range sliders

--- testbokeh.py
+++ testbokeh.py
@@ -8,7 +8,7 @@ import numpy.random as random
 
 from bokeh.io import curdoc
 from bokeh.layouts import row, widgetbox
-from bokeh.models import ColumnDataSource
+from bokeh.models import ColumnDataSource, Range1d
 from bokeh.models.widgets import Slider, TextInput
 from bokeh.plotting import figure
 
@@ -22,8 +22,10 @@ if useSerial:
 
 # Set up data
 channels = ["A","B","A'","B'"]
+coinc = ["C1","C2","C3","C4"]
 counts = [0,0,0,0]
 source = ColumnDataSource(data=dict(x=channels, y=counts))
+source2 = ColumnDataSource(data=dict(x=coinc, y=counts))
 
 # Set up plot
 plot = figure(plot_height=400, plot_width=400, title="raw counts",
@@ -32,26 +34,28 @@ plot = figure(plot_height=400, plot_width=400, title="raw counts",
 
 plot.vbar(x='x', top='y', width=0.5, source=source)
 
-#plot.bars('x', 'y', source=source, line_width=3, line_alpha=0.6)
-# plot = Bar(data, values='y', label='interpreter', stack='sample', agg='mean',
-#           title="Raw counts", legend='top_right', plot_width=400, plot_height=400)
+plot2 = figure(plot_height=400, plot_width=400, title="raw coinc",
+              tools="crosshair,pan,reset,save,wheel_zoom",
+              x_range=coinc, y_range=[0, 1000])
+
+plot2.vbar(x='x', top='y', width=0.5, source=source2)
 
 # Set up widgets
-text = TextInput(title="title", value='raw counts')
-offset = Slider(title="offset", value=0.0, start=-5.0, end=5.0, step=0.1)
-amplitude = Slider(title="amplitude", value=1.0, start=-5.0, end=5.0, step=0.1)
-phase = Slider(title="phase", value=0.0, start=0.0, end=2*np.pi)
+text = TextInput(title="Command Entry:", value='raw counts')
+scalemin = Slider(title="Scale minimum", value=0.0, start=0.0, end=5000.0, step=100)
+scalemax = Slider(title="Scale maximum", value=1000.0, start=1000.0, end=5500.0, step=100)
+phase = Slider(title="phase", value=0.0, start=0.0, end=2*np.pi, step=0.1)
 freq = Slider(title="frequency", value=1.0, start=0.1, end=5.1, step=0.1)
 
 
 # Set up callbacks
 def update_title(attrname, old, new):
+    #TODO turn into a raw command area?
     plot.title.text = text.value
 
 text.on_change('value', update_title)
 
 def update():
-
     # get data:
     if useSerial:
         s.write("c\n".encode())
@@ -65,48 +69,41 @@ def update():
     coinc = data[4:8]
     err = data[8]
 
-    # Generate the new curve
-    x = ["A","B","A'","B'"]
-    y = raw
+    #print(raw)
 
-    source.data = dict(x=x, y=y)
+    # Get the current slider values
+    # a = scalemax.value
+    # b = scalemin.value
+    # w = phase.value
+    # k = freq.value
+
+    # Generate the new curve
+    channels = ["A","B","A'","B'"]
+    chan2 = ["C1","C2","C3","C4"]
+
+    source.data = dict(x=channels, y=raw)
+    source2.data = dict(x=chan2, y=coinc)
 
 def update_data(attrname, old, new):
 
-    # get data:
-    if useSerial:
-        s.write("c\n".encode())
-        serialData = s.readline()
-        data = [int(x) for x in data.decode('ascii').rstrip().split(' ')]
-    else:
-        mockdata = [80000,75043,1000,800,20,20,0,0,0]
-        data = [random.rand()*x for x in mockdata]
-
-    raw = data[0:4]
-    coinc = data[4:8]
-    err = data[8]
-
-    print(raw)
-
     # Get the current slider values
-    a = amplitude.value
-    b = offset.value
+    a = scalemax.value
+    b = scalemin.value
     w = phase.value
     k = freq.value
 
-    # Generate the new curve
-    x = ["A","B","A'","B'"]
-    y = raw
+    #update()
 
-    source.data = dict(x=x, y=y)
+    plot2.y_range.start = b
+    plot2.y_range.end = a
 
-for w in [offset, amplitude, phase, freq]:
+for w in [scalemin, scalemax, phase, freq]:
     w.on_change('value', update_data)
 
 
 # Set up layouts and add to document
-inputs = widgetbox(text, offset, amplitude, phase, freq)
+inputs = widgetbox(text, scalemin, scalemax, phase, freq)
 
-curdoc().add_root(row(inputs, plot, width=800))
+curdoc().add_root(row(inputs, plot, plot2, width=1200))
 curdoc().title = "Sliders"
 curdoc().add_periodic_callback(update, 100)
